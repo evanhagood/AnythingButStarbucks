f2231603d448e385271c09f67c8450789beacd90
evanhagood <evanmhagood@gmail.com>
2025-10-13 20:51:27 -0400

    Initial Commit from Spring Initializr

---
+++ src/main/java/com/anythingbutstarbucks/AnythingButStarbucksApplication.java
@@ -0,0 +1,13 @@
+package com.anythingbutstarbucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnythingButStarbucksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnythingButStarbucksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anythingbutstarbucks/AnythingButStarbucksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anythingbutstarbucks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnythingButStarbucksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
